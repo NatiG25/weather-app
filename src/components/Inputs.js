6606eb738d193d669f7f284d21ed18e4be2ba288
NatiG <nati.g5684@gmail.com>
2023-04-12 01:01:29 +0300

    Create the inputs & TopButtons components

---
+++ src/components/Inputs.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ReactComponent as SearchIcon } from './icons/searchIcon.svg'
+import { ReactComponent as LocationIcon } from './icons/locationIcon.svg'
+
+function Inputs() {
+  return (
+    // <SearchIcon />
+    // <LocationIcon />
+    <div>
+        <div>
+            <input type='text' />
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Inputs
