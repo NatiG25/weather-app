812223df4f78b1372d4e6fd5a6ce899faf622acf
NatiG <nati.g5684@gmail.com>
2023-04-12 16:39:59 +0300

    Complete building the inputs component

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,18 +1,37 @@
-import React from 'react'
-import { ReactComponent as SearchIcon } from './icons/searchIcon.svg'
-import { ReactComponent as LocationIcon } from './icons/locationIcon.svg'
+import React from "react";
+import { BiSearchAlt } from "react-icons/bi";
+import { FiNavigation } from "react-icons/fi";
 
 function Inputs() {
   return (
-    // <SearchIcon />
-    // <LocationIcon />
-    <div>
-        <div>
-            <input type='text' />
-        </div>
-        <div></div>
+    <div className="flex flex-row justify-center my-6">
+      <div className="flex flex-row w-3/4 items-center justify-center space-x-2">
+        <input
+          type="text"
+          placeholder="Search for a city..."
+          className="text-xl font-light p-2 shadow-xl capitalize focus:outline-none placeholder:lowercase"
+        />
+        <BiSearchAlt
+          size={42}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+        />
+        <FiNavigation
+          size={42}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+        />
+      </div>
+
+      <div className="flex flex-row w-1/4 items-center justify-center">
+        <button name="metric" className="text-white text-xl font-light">
+          °C
+        </button>
+        <p className="text-xl text-white mx-1">|</p>
+        <button name="imperial" className="text-white text-xl font-light">
+          °F
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Inputs
+export default Inputs;
