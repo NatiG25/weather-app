afa1c613bb4ed201f612fe2b78dc47e6f2c0d888
NatiG <nati.g5684@gmail.com>
2023-04-26 04:07:05 +0300

    Import weather data to TimeAndLocation file

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { formatLocalTime } from "../weatherService";
 
-function TimeAndLocation() {
+function TimeAndLocation({weather: {dt, timezone, name, country}}) {
   return (
     <div>
       <div className="flex justify-center items-center my-6">
         <p className="text-white font-extralight text-xl">
-          Tuesday, 31 May 2022 | Local time: 12:39 PM
+          {formatLocalTime(dt, timezone)}
         </p>
       </div>
 
       <div className="flex justify-center items-center my-3">
-        <p className="text-white font-medirm text-3xl">Berlin, DE</p>
+        <p className="text-white font-medirm text-3xl">{`${name}, ${country}`}</p>
       </div>
     </div>
   );
