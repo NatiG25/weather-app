b403547f2455fc21b750c429321328bb3894a2b8
NatiG <nati.g5684@gmail.com>
2023-04-27 03:19:48 +0300

    Use react toastify to make popup messages for every API request

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import Forecast from "./components/Forecast";
 import getFormattedWeatherData from "./weatherService";
 import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { v4 } from "uuid";
 
 function App() {
   const [query, setQuery] = useState({ q: "tokyo" });
@@ -14,9 +17,15 @@ function App() {
 
   useEffect(() => {
     const fetchWeather = async () => {
-      await getFormattedWeatherData({ ...query, units }).then((data) =>
-        setWeather(data)
-      );
+      const messgae = query.q ? query.q : "current location.";
+
+      toast.info("Fetching data for " + messgae);
+
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        toast.success(`Successfully featched ${data.name}'s weather.`);
+
+        setWeather(data);
+      });
     };
 
     fetchWeather();
@@ -24,8 +33,21 @@ function App() {
 
   console.log(weather);
 
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-700 to-blue-700";
+
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) {
+      return "from-cyan-700 to-blue-700";
+    } else {
+      return "from-yellow-700 to-orange-700";
+    }
+  };
+
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+    <div
+      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
+    >
       <TopButtons setQuery={setQuery} />
       <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 
@@ -34,10 +56,12 @@ function App() {
           <TimeAndLocation weather={weather} />
           <TemperatureAndDetails weather={weather} />
 
-          <Forecast title={"Hourly Forecast"} items={weather.hourly} />
-          <Forecast title={"Daily Forecast"} items={weather.daily} />
+          <Forecast title={"Hourly Forecast"} key={v4()} items={weather.hourly} />
+          <Forecast title={"Daily Forecast"} key={v4()} items={weather.daily} />
         </>
       )}
+
+      <ToastContainer autoClose={4000} theme="colored" newestOnTop={true} />
     </div>
   );
 }
